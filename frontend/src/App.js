2d2ac6fa7dce59e6b072f271781090bfa6a0cda5
neylanepl <neylanepl@gmail.com>
2024-05-30 19:57:55 -0300

    feat: estrutura de rotas da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import PrivateRoute from './pages/base/privateRoute.js';
+import Inicio from './pages/inicio/inicio.js';
+
+import CadastrarBovino from './pages/bovinos/cadastrarBovino.js';
+import EditarBovino from './pages/bovinos/editarBovino.js';
+import ListarBovino from './pages/bovinos/listarBovino.js';
+import TelaListarCarteiraBovino from './pages/bovinos/telaListagemCarteira.js';
+import CarteiraVacinacaoBovino from './pages/bovinos/carteiraVacinacao.js';
+
+import CadastrarVacina from './pages/vacinas/cadastrarVacinas.js';
+import EditarVacina from './pages/vacinas/editarVacinas.js';
+import ListarVacina from './pages/vacinas/listarVacinas.js';
+
+import CadastrarDose from './pages/dose/cadastrarDose.js';
+import EditarDose from './pages/dose/editarDose.js';
+import ListarDose from './pages/dose/listarDose.js';
+
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <Routes>
+        <Route path="/" element={<PrivateRoute><Inicio /></PrivateRoute>} />
+
+        <Route path="/bovinos" element={<PrivateRoute><ListarBovino /></PrivateRoute>} />
+        <Route path="/bovinos/cadastrarBovino" element={<PrivateRoute><CadastrarBovino /></PrivateRoute>} />
+        <Route path="/bovinos/editarBovino/:id" element={<PrivateRoute><EditarBovino /></PrivateRoute>} />
+        <Route path="/bovinos/telaListagemCarteira" element={<TelaListarCarteiraBovino />} />
+        <Route path="/bovinos/carteiraVacinacao" element={<CarteiraVacinacaoBovino />} />
+
+        <Route path="/vacinas" element={<PrivateRoute><ListarVacina /></PrivateRoute>} />
+        <Route path="/vacinas/cadastrarVacina" element={<PrivateRoute><CadastrarVacina /></PrivateRoute>} />
+        <Route path="/vacinas/editarVacina/:nome_vacina" element={<PrivateRoute><EditarVacina /></PrivateRoute>} />
+
+        <Route path="/doses" element={<PrivateRoute><ListarDose /></PrivateRoute>} />
+        <Route path="/doses/cadastrarDose" element={<PrivateRoute><CadastrarDose /></PrivateRoute>} />
+        <Route path="/doses/editarDose" element={<PrivateRoute><EditarDose /></PrivateRoute>} />
+
+      </Routes>
+      <ToastContainer />
+    </Router>
+
   );
 }
 
